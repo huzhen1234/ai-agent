86bd034bb170804264b3b2d2de06e6f4cd8af652
huzhen <huzhen@bestpay.com.cn>
2025-07-14 18:06:46 +0800

    RAG

--- src/test/java/com/hutu/aiagent/AiAgentApplicationTests.java
+++ src/test/java/com/hutu/aiagent/AiAgentApplicationTests.java
@@ -1,13 +1,27 @@
 package com.hutu.aiagent;
 
+import cn.hutool.json.JSONUtil;
+import com.hutu.aiagent.utils.LoveAppDocumentLoader;
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.document.Document;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class AiAgentApplicationTests {
 
+    @Resource
+    private LoveAppDocumentLoader loveAppDocumentLoader;
+
     @Test
     void contextLoads() {
+        List<Document> documents = loveAppDocumentLoader.loadMarkdowns();
+        for (Document document : documents) {
+            System.out.println(JSONUtil.toJsonStr(document));
+        }
+
     }
 
 }
