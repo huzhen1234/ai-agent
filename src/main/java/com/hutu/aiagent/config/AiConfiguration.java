ad9193773e6c5995ba270a94e422dc9ee99403d2
huzhen <huzhen@bestpay.com.cn>
2025-07-15 17:21:38 +0800

    RAG演示

--- src/main/java/com/hutu/aiagent/config/AiConfiguration.java
+++ src/main/java/com/hutu/aiagent/config/AiConfiguration.java
@@ -58,15 +58,59 @@ public class AiConfiguration {
     }
 
     @Bean
-    public ChatClient chatClient(DashScopeChatModel dashScopeChatModel, ChatMemory chatMemory) {
+    public ChatClient chatClient(DashScopeChatModel dashScopeChatModel, ChatMemory chatMemory, SimpleVectorStore simpleVectorStore) {
         return ChatClient
                 .builder(dashScopeChatModel)
-                .defaultSystem("你是一名高级开发人员，请你依照你的经验提供合理的方案")
+                .defaultSystem("""
+						您是“Funnair”航空公司的客户聊天支持代理。请以友好、乐于助人且愉快的方式来回复。
+						您正在通过在线聊天系统与客户互动。
+						您能够支持已有机票的预订详情查询、机票日期改签、机票预订取消等操作，其余功能将在后续版本中添加，如果用户问的问题不支持请告知详情。
+						在提供有关机票预订详情查询、机票日期改签、机票预订取消等操作之前，您必须始终从用户处获取以下信息：预订号、客户姓名。
+						在询问用户之前，请检查消息历史记录以获取预订号、客户姓名等信息，尽量避免重复询问给用户造成困扰。
+						在更改预订之前，您必须确保条款允许这样做。
+						如果更改需要收费，您必须在继续之前征得用户同意。
+						使用提供的功能获取预订详细信息、更改预订和取消预订。
+						如果需要，您可以调用相应函数辅助完成。
+						请讲中文。
+
+						今天的日期是 {current_date}.
+					""")
                 // 环绕增强
                 .defaultAdvisors(
                         new SimpleLoggerAdvisor(),// 开启日志 此时需要配置日志级别
-                        MessageChatMemoryAdvisor.builder(chatMemory).build() // 开启内存存储
-                )
+                        MessageChatMemoryAdvisor.builder(chatMemory).build(), // 开启内存存储 -- 聊天记录
+//                        PromptChatMemoryAdvisor.builder(chatMemory).build(),  // 开启内存存储 -- 提示词(精简般的聊天记录，记录核心内容)
+                        new QuestionAnswerAdvisor(simpleVectorStore)  // RAG 询问时，会先想RAG数据库查询
+//						VectorStoreChatMemoryAdvisor.builder(simpleVectorStore).build() // 开启内存存储 -- RAG数据库
+                )/*.defaultToolNames(
+						"getBookingDetails",
+						"changeBooking",
+						"cancelBooking"
+				)*/
                 .build();
     }
+
+	/**
+	 * 		return this.chatClient.prompt()
+	 * 				.system(s -> s.param("current_date", LocalDate.now().toString()))
+	 */
+
+
+    /**
+     * List<Message> history = [
+     *     UserMessage("我想订明天北京飞上海的航班..."),
+     *     AiMessage("已找到3个航班：CA1855...")
+     * ];
+     *
+     *
+     * 存储提取后的关键信息（需自定义提取逻辑）
+     * List<String> prompts = [
+     *     "出发地:北京",
+     *     "目的地:上海",
+     *     "日期:2024-07-20",
+     *     "舱位:经济舱",
+     *     "偏好:靠窗",
+     *     "可选航班:CA1855,MU511,HU760"
+     * ];
+     */
 }
--- src/main/java/com/hutu/aiagent/config/VectorStoreConfig.java
+++ src/main/java/com/hutu/aiagent/config/VectorStoreConfig.java
@@ -1,15 +1,29 @@
 package com.hutu.aiagent.config;
 
+import lombok.SneakyThrows;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 public class VectorStoreConfig {
     
+    @SneakyThrows
     @Bean
-    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel) {
-        return SimpleVectorStore.builder(embeddingModel).build();
+    public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel,@Value("classpath:rag/jipiao.txt") Resource termsOfServiceDocs) {
+        SimpleVectorStore vectorStore = SimpleVectorStore.builder(embeddingModel).build();
+        // 读取文本内容
+        String content = StreamUtils.copyToString(termsOfServiceDocs.getInputStream(), StandardCharsets.UTF_8);
+        // 添加文档到向量库
+        vectorStore.add(List.of(Document.builder().text(content).build()));
+        return vectorStore;
     }
 }
